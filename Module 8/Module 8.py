197c8c2f9d403e9658344a06719d3b8b51bf8a0a
AlexanderLitovchenko <111634079+AlexanderLitovchenko@users.noreply.github.com>
2022-10-30 14:43:26 +0300

    Update Module 8.py

--- Module 8/Module 8.py
+++ Module 8/Module 8.py
@@ -3,8 +3,8 @@ import sqlite3
 conn = sqlite3.connect('database.sqlite')
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS Students (id INT PRIMARY KEY, name Varchar(32), surname Varchar(32), age INT, city Varchar(32))")
-cursor.execute("CREATE TABLE IF NOT EXISTS Courses (id INT PRIMARY KEY, name Varchar(32), time_start Varchar(32), time_end  Varchar(32))")
+cursor.execute("CREATE TABLE Students (id INT PRIMARY KEY, name Varchar(32), surname Varchar(32), age INT, city Varchar(32))")
+cursor.execute("CREATE TABLE Courses (id INT PRIMARY KEY, name Varchar(32), time_start Varchar(32), time_end  Varchar(32))")
 
 cursor.executemany("INSERT INTO Students (id, name, surname, age, city) VALUES (?, ?, ?, ?, ?)", 
 [(1, 'Max', 'Brooks', 24, 'Spb'),
@@ -16,7 +16,7 @@ cursor.executemany('''INSERT INTO Courses VALUES (?, ?, ?, ?)''',
 [(1, 'python', '21.07.21', '21.08.21'),
 (2, 'java', '13.07.21', '16.08.21')])
 
-cursor.execute("CREATE TABLE IF NOT EXISTS Student_courses (student_id INT, course_id INT, FOREIGN KEY(student_id) REFERENCES Students(id), FOREIGN KEY(course_id) REFERENCES Courses(id))")
+cursor.execute("CREATE TABLE Student_courses (student_id INT, course_id INT, FOREIGN KEY(student_id) REFERENCES Students(id), FOREIGN KEY(course_id) REFERENCES Courses(id))")
 cursor.executemany('''INSERT INTO Student_courses VALUES (?, ?)''',
 [(1, 1), (2, 1), (3, 1), (4, 2)])
 
